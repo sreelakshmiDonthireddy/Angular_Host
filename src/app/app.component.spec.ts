dc47a0ceba8e16884029d14f974c553094e6affd
sreelakshmid <sreelakshmid.in@mouritech.com>
2024-02-22 13:02:14 +0530

    Initial Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular_MSAL'`, () => {
+  it(`should have as title 'azuread-angular-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular_MSAL');
+    expect(app.title).toEqual('azuread-angular-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular_MSAL app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('azuread-angular-demo app is running!');
   });
 });
